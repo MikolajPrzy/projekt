b98233b5662f31ebb20610f4b785fb2aba355021
student <251252@edu.p.lodz.pl>
2023-12-17 17:00:51 +0100

    01 Zmiana na mnożenie  Policz  (przepisane według instrukcji )

--- main.c
+++ main.c
@@ -1,54 +1,10 @@
-/*#include <stdio.h>
+#include <iostream>
 
-int main() {
-    int n, count = 0;
-
-    printf("Podaj liczbe n: ");
-    scanf("%d", &n);
-
-
-    for (int i = 1; i < n; i++) {
-
-        if (i % 5 == 0 && i % 3 != 0) {
-            count++;
-        }
-    }
-
-    printf(" liczb mniejszych od %d, podzielnych przez 5 i niepodzielnych przez 3: %d\n", n, count);
-
-    return 0;
+int Policz( int a, int b) {
+    return a * b;
 }
- */
-
-#include <stdio.h>
-
-
-double horner(double coef[], int n, double x) {
-    if (n == 0) {
-        return coef[0];
-    }
-    return coef[n] + x * horner(coef, n - 1, x);
-}
-
-int main() {
-    int n, i;
-    double x, coef[100];
-
-    printf("Podaj stopien wielomianu (maks. 99): ");
-    scanf("%d", &n);
-
-    printf("Podaj wspolczynniki wielomianu (od najwyzszej potegi do stalej): \n");
-    for (i = n; i >= 0; i--) {
-        printf("x^%d: ", i);
-        scanf("%lf", &coef[i]);
-    }
-
-    printf("Podaj wartosc argumentu x: ");
-    scanf("%lf", &x);
-
-
-    double result = horner(coef, n, x);
-    printf("Wartosc wielomianu dla x = %.2lf wynosi: %.2lf\n", x, result);
-
+int main(){
+    std:: cout << "Hello, world!" << std ::endl;
+    std::cout << "W pierwszym branchu; 2+3=" << Policz(2,3)) << std::end
     return 0;
 }
